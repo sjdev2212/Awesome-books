7a859d6687576b8f1faf4a4823fdc98280e60a6e
Anibal Amoroso <anibaldaniel2212@gmail.com>
2021-10-20 18:17:39 -0300

    Fix localstorage and javascript issues

--- index.js
+++ index.js
@@ -2,68 +2,72 @@ const input = document.querySelector('.input-1');
 const input2 = document.getElementById('input2');
 const addBtn = document.querySelector('.btn-add');
 const ul = document.querySelector('ul');
-const form = document.getElementById('form');
-const empty = document.querySelector('.empty');
 
-// const collection = localStorage.getItem("book-authors") ;
-const collection  = [];
-function Awesome(book, author) {
-  this.book = book;
+let collection = JSON.parse(localStorage.getItem('book-authors'));
+
+function Awesome(title, author, id) {
+  this.title = title;
   this.author = author;
+  this.id = id;
 }
 
-function addDeleteBtn() {
-  const deleteBtn = document.createElement('button');
+function displayBooks() {
+  if (ul.querySelectorAll('li')) {
+    Array.from(ul.querySelectorAll('li')).forEach((bookContainer) => {
+      ul.removeChild(bookContainer);
+    });
+  }
 
-  deleteBtn.textContent = 'Delete';
-  deleteBtn.className = 'btn-delete';
-
-  deleteBtn.addEventListener('click', (e) => {
-    const item = e.target.parentElement;
-    ul.removeChild(item);
-
-    const items = document.querySelectorAll('li');
-
-    if (items.length === 0) {
-      empty.style.display = 'block';
-    }
-  });
-
-  return deleteBtn;
-}
-
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-
-  const inputs = new Awesome(input.value, input2.value);
-  collection.push(inputs);
-  
-
-  const text = input.value;
-  const text2 = input2.value;
-
-  if (text !== '') {
+  collection.forEach((book) => {
     const li = document.createElement('li');
     li.className = 'books-li';
-    const p = document.createElement('p');
-    p.className = ".paragraph-1"
-    const secondParagraph = document.createElement('p');
-    secondParagraph.className = "paragraph-2";
+    const author = document.createElement('p');
+    author.className = 'paragraph-1';
+    const bookName = document.createElement('p');
+    bookName.className = 'paragraph-2';
+    const deleteButton = document.createElement('button');
+    deleteButton.id = book.id;
+    deleteButton.className = 'remove-btn';
+    deleteButton.textContent = 'remove';
 
-    p.textContent = text;
-    secondParagraph.textContent = text2;
+    author.textContent = book.author;
+    bookName.textContent = book.title;
 
-    li.appendChild(p);
-    li.appendChild(secondParagraph);
-    li.appendChild(addDeleteBtn());
+    li.appendChild(author);
+    li.appendChild(bookName);
+    li.appendChild(deleteButton);
     ul.appendChild(li);
+  });
+  // eslint-disable-next-line no-use-before-define
+  assignRemoveBtn();
+}
+displayBooks();
 
-    input.value = '';
-    empty.style.display = 'none';
-    localStorage.setItem("book-authors",JSON.stringify(collection))
+function assignRemoveBtn() {
+  if (collection.length) {
+    const removeBtns = Array.from(document.querySelectorAll('.remove-btn'));
+
+    removeBtns.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        const array = [];
+        collection.forEach((book) => {
+          if (parseInt(btn.id, 10) !== book.id) {
+            array.push(book);
+          }
+        });
+
+        collection = array;
+        localStorage.setItem('book-authors', JSON.stringify(collection));
+        displayBooks();
+      });
+    });
   }
+}
+
+addBtn.addEventListener('click', () => {
+  const inputs = new Awesome(input.value, input2.value, collection.length + 1);
+  collection.push(inputs);
+
+  localStorage.setItem('book-authors', JSON.stringify(collection));
+  displayBooks();
 });
-
-
-
-
